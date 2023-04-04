be57ebdca8593257c50220d9af52b2f7bec34184
Mglod-usernametaken <masterwloczykij@gmail.com>
2023-04-05 00:26:48 +0200

    add backrground, banner movement

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Python version : 3.10.10
 PyGame version : 2.1.2
 Sprite Size : TBD (32 or 48, imo no reason to go higher)
-Default Resolution : 1600 x 1200
+Default Resolution : 1440 x 900
--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 import pygame
 
 
-SCREEN_HEIGHT = 1200
-SCREEN_WIDTH = 1600
+SCREEN_HEIGHT = 900
+SCREEN_WIDTH = 1440
 
+#initial setup
+pygame.init()
 screen = pygame.display.set_mode ((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('Pley the gem')
+pygame.display.set_caption('The gem')
+clock = pygame.time.Clock()
+test_font = pygame.font.Font(None, 50)
 
+#surfaces
+screen.fill('#3b3d40')
+background = pygame.Surface((1440,800))
+background = pygame.image.load('sprites/test.png')
+
+banner = pygame.Surface((900,200))
+banner = pygame.image.load('sprites/test-banner.png')
+banner_y_pos = 700
+
+text_surface = test_font.render('this text is rendered by pygame', True, 'crimson')
+
+#main loop
 run = True
 while run:
     
@@ -16,6 +32,13 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
-    pygame.display.update()
+    screen.blit(background,(0,0))
+    screen.blit(banner,(270,banner_y_pos))
+    banner_y_pos -= 1
+    if banner_y_pos <= -200:
+        banner_y_pos = 900
+    screen.blit(text_surface,(720,300))
 
+    pygame.display.update()
+    clock.tick(60)
 pygame.quit()
